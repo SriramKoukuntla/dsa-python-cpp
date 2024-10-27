54c61def430e3742615624b5813352e22012e02f
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2024-10-27 19:06:44 -0400

    Redid217ContainsDuplicates

---
+++ Leetcode Easy/217ContainsDuplicate.py
@@ -0,0 +1,18 @@
+#https://leetcode.com/problems/contains-duplicate/
+# class Solution(object):
+#     def containsDuplicate(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: bool
+#         """
+#         hashset = set()
+#         for i in nums:
+#             if i in hashset:
+#                 return True
+#             hashset.add(i)
+#         return False
+
+#Better way
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return len(set(nums)) != len(nums)

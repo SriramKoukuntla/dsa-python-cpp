9c0be125d6beb4eb0cf18ac7ccec8b2110a3345f
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2024-11-04 10:52:18 -0500

    977SquaresOfSortedArray But Optimized Time Complexity

--- Leetcode Easy/977SquaresOfSortedArray.py
+++ Leetcode Easy/977SquaresOfSortedArray.py
@@ -1,3 +1,15 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        return sorted(list(map(lambda x: x*x, nums)))
+        # return sorted(list(map(lambda x: x*x, nums)))
+        #This is O(log(N)) solution
+        left, right = 0, len(nums)-1
+        ret = []
+        while left <= right:
+            if abs(nums[left]) > abs(nums[right]):
+                ret.append(nums[left]**2)
+                left += 1
+            else:
+                ret.append(nums[right]**2)
+                right -= 1
+        return ret[::-1]
+        #This is O(N) solution

d7cc0e61cb5b4293ed451ebf7f0ff35146b79817
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2024-12-31 16:55:40 -0600

    InsertIntoABinaryTree

---
+++ Leetcode Medium/701InsertIntoBinaryTree.py
@@ -0,0 +1,27 @@
+#https://leetcode.com/problems/insert-into-a-binary-search-tree/
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        directions = deque([])
+        curr = root
+        while curr != None:
+            if val < curr.val:
+                curr = curr.left
+                directions.append("left")
+            else: 
+                curr = curr.right
+                directions.append("right")
+        curr = root
+        while len(directions) > 1:
+            direction = directions.popleft()
+            if direction == "left": curr = curr.left
+            else: curr = curr.right
+        if curr == None: return TreeNode(val)
+        if directions.popleft() == "left": curr.left = TreeNode(val)
+        else: curr.right = TreeNode(val)
+        return root

86d6aa72e785330407b20fc7adf2cd30d379f717
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2025-02-03 09:36:54 -0500

    124BinaryMaximumPathSum

---
+++ Leetcode Hard/124BinaryMaximumPathSum.py
@@ -0,0 +1,34 @@
+#https://leetcode.com/problems/binary-tree-maximum-path-sum/description/
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        maxRet = float('-inf')
+        def helper(node):
+            nonlocal maxRet
+            if node == None:
+                return None
+            if node.left == None and node.right == None:
+                maxRet = max(maxRet, node.val)
+                return node.val
+            if node.left != None and node.right == None:
+                left = helper(node.left)
+                temp = max(node.val, node.val+left)
+                maxRet = max(maxRet, temp)
+                return temp
+            if node.left == None and node.right != None:
+                right = helper(node.right)
+                temp = max(node.val, node.val+right)
+                maxRet = max(maxRet, temp)
+                return temp
+            if node.left != None and node.right != None:
+                left, right = helper(node.left), helper(node.right)
+                temp = max(node.val, max(node.val + left, node.val + right))
+                maxRet = max(maxRet, max(temp, node.val + left + right))
+                return temp
+        helper(root)
+        return maxRet

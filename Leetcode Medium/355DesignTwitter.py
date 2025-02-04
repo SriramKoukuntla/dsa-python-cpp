b1aea19445776a5fc260cf9773fd3fa437d9b2d1
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2025-02-03 22:58:26 -0500

    355DesignTwitter

---
+++ Leetcode Medium/355DesignTwitter.py
@@ -0,0 +1,41 @@
+#https://leetcode.com/problems/design-twitter/
+from collections import defaultdict, deque
+class Twitter:
+
+    def __init__(self):
+        self.time = 0
+        self.following = defaultdict(set) #key = userId, value = set() of users they follow
+        self.posts = defaultdict(deque) #key = userId, value = queue of posts and their time
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        self.following[userId].add(userId)
+        self.posts[userId].append((self.time, tweetId))
+        if len(self.posts[userId]) > 10:
+            self.posts[userId].popleft()
+        self.time+=1
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        allPosts = []
+        for influencer in self.following[userId]:
+            allPosts += self.posts[influencer]
+        heapq.heapify(allPosts)
+        ret = []
+        length = min(10, len(allPosts))
+        for i in range(length):
+            ret.append(heapq.heappop(allPosts)[1])
+        return ret[::-1]
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        self.following[followerId].add(followeeId)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        self.following[followerId].remove(followeeId)
+
+
+
+# Your Twitter object will be instantiated and called as such:
+# obj = Twitter()
+# obj.postTweet(userId,tweetId)
+# param_2 = obj.getNewsFeed(userId)
+# obj.follow(followerId,followeeId)
+# obj.unfollow(followerId,followeeId)

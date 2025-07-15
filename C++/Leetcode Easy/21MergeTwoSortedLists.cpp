1eb8e3d29507272dcadab144b31a4bbd6d6678ca
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2025-07-15 19:21:39 -0400

    21MergeTwoSortedListsCPP Recursive

--- C++/Leetcode Easy/21MergeTwoSortedLists.cpp
+++ C++/Leetcode Easy/21MergeTwoSortedLists.cpp
@@ -30,3 +30,15 @@ public:
         return result;
     }
 };
+
+//Recursive
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if (list1 == nullptr) return list2;
+        if (list2 == nullptr) return list1;
+        return (list1->val < list2->val)
+         ? (list1->next = mergeTwoLists(list1->next, list2), list1)
+         : (list2->next = mergeTwoLists(list1, list2->next), list2);
+    }
+};

1fcc286d325f17637163640fb97e23d93863b832
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2025-07-15 19:44:32 -0400

    21MergeTwoSortedListsCPP NoDummyNode

--- C++/Leetcode Easy/21MergeTwoSortedLists.cpp
+++ C++/Leetcode Easy/21MergeTwoSortedLists.cpp
@@ -42,3 +42,39 @@ public:
          : (list2->next = mergeTwoLists(list1, list2->next), list2);
     }
 };
+
+//No dummy node
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if (list1 == nullptr) return list2;
+        if (list2 == nullptr) return list1;
+
+        ListNode* head;
+        ListNode* tail;
+
+        if (list1->val < list2->val){
+            head = tail = list1;
+            list1 = list1->next;
+        }
+        else{
+            head = tail = list2;
+            list2 = list2->next;
+        }
+
+        while (list1 != nullptr && list2 != nullptr){
+            if (list1->val < list2->val){
+                tail->next = list1;
+                list1 = list1->next;
+            }
+            else{
+                tail->next = list2;
+                list2 = list2->next;
+            }
+            tail = tail->next;
+        }
+
+        tail->next = list1 ? list1 : list2;
+        return head;
+    }
+};

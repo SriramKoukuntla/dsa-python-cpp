0e41ae010144b9cc57a528c06e223f6a17b73965
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2025-07-15 22:19:29 -0400

    543DiameterOfBinaryTreeCPP

---
+++ C++/Leetcode Easy/543DiameterOfBinaryTree.cpp
@@ -0,0 +1,31 @@
+#include <algorithm>
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int res;
+
+    int diameterOfBinaryTree(TreeNode* root) {
+        res = 0;
+        helper(root);
+        return res;
+    }
+
+private:
+    int helper(TreeNode* root){
+        if (!root) return 0;
+        int left = helper(root->left);
+        int right = helper(root->right);
+        res = max(res, left+right);
+        return 1 + max(left, right);
+    }
+};

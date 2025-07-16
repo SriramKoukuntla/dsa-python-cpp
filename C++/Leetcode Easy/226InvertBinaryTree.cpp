f8a5a451509677e9ee2834654691cf608f5508e7
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2025-07-15 20:42:00 -0400

    226InvertBinaryTreeCPPRecursive

--- C++/Leetcode Easy/226InvertBinaryTree.cpp
+++ C++/Leetcode Easy/226InvertBinaryTree.cpp
@@ -29,3 +29,16 @@ public:
         return root;
     }
 };
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (root == nullptr) return root;
+        TreeNode* temp = root->left;
+        root->left = root->right;
+        root->right = temp;
+        invertTree(root->left); 
+        invertTree(root->right); 
+        return root;
+    }
+};

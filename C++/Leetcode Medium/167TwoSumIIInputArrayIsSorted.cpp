0e92a40d6b793fc6709b767fb2f3ec32d2ad4522
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2025-07-19 11:43:43 -0400

    167TwoSumIIInputArrayIsSorted

---
+++ C++/Leetcode Medium/167TwoSumIIInputArrayIsSorted.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int begin = 0;
+        int end = numbers.size()-1;
+        int sum = numbers[begin] + numbers[end];
+        while (sum != target){
+            if (sum > target) --end;
+            else ++begin;
+            sum = numbers[begin] + numbers[end];
+        }
+        return {begin+1, end+1};
+    }
+};

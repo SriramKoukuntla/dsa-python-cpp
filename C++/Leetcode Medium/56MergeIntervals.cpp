f93da30dfdc32bdd817137f9fb62e9faedd38f0c
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2025-10-29 16:00:15 -0400

    Improved 56MergeIntervalsCPP

--- C++/Leetcode Medium/56MergeIntervals.cpp
+++ C++/Leetcode Medium/56MergeIntervals.cpp
@@ -1,3 +1,28 @@
+#include <deque>
+#include <vector>
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end(), [](vector<int>& a, vector<int>& b){return a[0] < b[0];});
+
+        vector<vector<int>> res;
+        res.push_back(intervals[0]);
+
+        for (int i = 1; i < intervals.size(); ++i){
+            if (res.back()[1] >= intervals[i][0]){
+                res.back()[1] = max(res.back()[1], intervals[i][1]);
+            }
+            else{
+                res.push_back(intervals[i]);
+            }
+        }
+
+        return res;
+    }
+};
+
+
 #include <vector>
 #include <deque>
 class Solution {

d5582ad3f2c54397d4749152e7b19b2dcd80f2c7
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2026-01-09 21:59:24 -0600

    20ValidParenthesisCPPRedid

--- C++/Leetcode Easy/20ValidParenthesis.cpp
+++ C++/Leetcode Easy/20ValidParenthesis.cpp
@@ -1,6 +1,29 @@
 #include <vector>
 #include <string>
 using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+        vector<char> stack;
+        for (int i = 0; i < s.size(); ++i){
+            if (s[i] == '(' || s[i] == '{' || s[i] == '[') stack.push_back(s[i]);
+            else {
+                if (stack.empty()) return false;
+                if (s[i] == ')' && stack.back() != '(') return false;
+                else if (s[i] == ']' && stack.back() != '[') return false;
+                else if (s[i] == '}' && stack.back() != '{') return false;
+                stack.pop_back();
+            }
+        }
+        return stack.empty();
+    }
+};
+
+
+#include <vector>
+#include <string>
+using namespace std;
 class Solution {
 public:
     bool isValid(string s) {

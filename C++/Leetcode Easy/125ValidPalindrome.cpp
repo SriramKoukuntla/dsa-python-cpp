d2a8f292fb56488ce567add69f78a525444a1dac
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2026-01-09 21:02:05 -0600

    125ValidPalindomeCPPRedid

--- C++/Leetcode Easy/125ValidPalindrome.cpp
+++ C++/Leetcode Easy/125ValidPalindrome.cpp
@@ -1,5 +1,24 @@
 #include <string>
 #include <cctype>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string newS = "";
+        for (int i = 0; i < s.length(); ++i){
+            if (isalnum(s[i])) newS += tolower(s[i]);
+        }
+        for (int i = 0; i < newS.length()/2; ++i){
+            if (newS[i] != newS[newS.length() - 1- i]) return false;
+        }
+        return true;
+    }
+};
+
+#include <string>
+#include <cctype>
 using namespace std;
 
 class Solution{

9d4ed50d4aad0fe7e3c5a6561a6aa3acb0e1a41b
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2026-01-13 15:15:49 -0600

    155MinStackCPPRedid

--- C++/Leetcode Medium/155MinStack.cpp
+++ C++/Leetcode Medium/155MinStack.cpp
@@ -1,4 +1,47 @@
 #include <vector>
+#include <algorithm>
+using namespace std;
+
+class MinStack {
+public:
+    vector<int> stack;
+    vector<int> minStack;
+    MinStack() {
+
+    }
+    
+    void push(int val) {
+        stack.push_back(val);
+        if (minStack.empty()) minStack.push_back(val);
+        else {
+            if (val <= minStack.back()) minStack.push_back(val);
+        }
+    }
+    
+    void pop() {
+        if (stack.back() == minStack.back()) minStack.pop_back();
+        stack.pop_back();
+    }
+    
+    int top() {
+        return stack.back();
+    }
+    
+    int getMin() {
+        return minStack.back();
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
+
+#include <vector>
 #include <tuple>
 using namespace std;
 class MinStack {

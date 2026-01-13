f95ecd09af1adbeba67fa4d65269a5ead8dece39
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2026-01-13 05:03:58 -0600

    242ValidAnagramCppRedid

--- C++/Leetcode Easy/242ValidAnagram.cpp
+++ C++/Leetcode Easy/242ValidAnagram.cpp
@@ -1,4 +1,41 @@
 #include <unordered_map>
+using namespace std;
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.size() != t.size()) return false;
+
+        unordered_map<char, int> m1;
+        for (int i = 0; i < s.size(); ++i){
+            ++m1[s[i]];
+            --m1[t[i]];
+        }
+
+        for (auto& [c, i] : m1) if (i != 0) return false;
+        return true;
+    }
+};
+
+#include <map>
+using namespace std;
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.size() != t.size()) return false;
+
+        map<char, int> m1;
+        map<char, int> m2;
+        for (int i = 0; i < s.size(); ++i){
+            ++m1[s[i]];
+            ++m2[t[i]];
+        }
+
+        return m1==m2;
+    }
+};
+
+
+#include <unordered_map>
 #include <iterator>
 using namespace std;
 

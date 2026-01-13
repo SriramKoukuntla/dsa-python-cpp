09c916e6ca13b9400796a23c32a0f5f190b3b40c
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2026-01-13 01:24:13 -0600

    206ReverseLinkedListCppRedid

--- C++/Leetcode Easy/206ReverseLinkedList.cpp
+++ C++/Leetcode Easy/206ReverseLinkedList.cpp
@@ -6,6 +6,62 @@
  *     ListNode() : val(0), next(nullptr) {}
  *     ListNode(int x) : val(x), next(nullptr) {}
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        return helper(head, nullptr);   
+    }
+
+    ListNode* helper(ListNode* head, ListNode* prev) {
+        if (head == nullptr) return head;
+        if (head->next == nullptr) {
+            head->next = prev;
+            return head;
+        }
+        ListNode* next = head->next;
+        head->next = prev;
+        return helper(next, head);
+    }
+
+};
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (head == nullptr) return head;
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+        while (curr->next) {
+            ListNode* next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;            
+        }
+        curr->next = prev;
+        return curr;
+    }
+};
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };06
  */
 class Solution {

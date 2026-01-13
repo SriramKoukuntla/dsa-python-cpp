a1827eb0cd1d4dd811bc8fda6a49d249e69bbc8d
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2026-01-13 01:56:14 -0600

    226InvertBinaryTreeCPPRedid

--- C++/Leetcode Easy/226InvertBinaryTree.cpp
+++ C++/Leetcode Easy/226InvertBinaryTree.cpp
@@ -1,3 +1,61 @@
+//2 ways do both
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (root == nullptr) return root;
+        if (root->left) invertTree(root->left);
+        if (root->right) invertTree(root->right);
+        TreeNode* temp= root->left;
+        root->left = root->right;
+        root->right = temp;
+        return root;
+    }
+};
+
+#include <list>
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        list<TreeNode*> queue;
+        queue.push_back(root);
+        while (queue.size() != 0) {
+            TreeNode* curr = queue.front();
+            queue.pop_front();
+            if (!curr) continue;
+            TreeNode* Temp = curr->left;
+            curr->left = curr->right;
+            curr->right = Temp;
+            queue.push_back(curr->left);
+            queue.push_back(curr->right);
+        }
+        return root;
+    }
+};
+
+
 #include <queue>
 using namespace std;
 /**

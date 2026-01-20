b2680479b8b2c618a2ee48c17533d1865fbc4bb0
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2026-01-20 05:33:40 -0600

    15ThreeSumCppRedid

--- C++/Leetcode Medium/15ThreeSum.cpp
+++ C++/Leetcode Medium/15ThreeSum.cpp
@@ -1,4 +1,32 @@
 #include <vector>
+using namespace std;
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> res;
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size(); ++i) {
+            if (i != 0 && nums[i] == nums[i-1]) continue;
+            if (nums[i] > 0) continue;
+            int l = i+1;
+            int r = nums.size()-1;
+            while (l < r) {
+                int sum = nums[i] + nums[l] + nums[r];
+                if (sum == 0) {
+                    res.push_back({nums[i], nums[l], nums[r]});
+                    ++l;
+                    --r;
+                    while (l < r && nums[l-1] == nums[l]) ++l;
+                    while (l < r && nums[r+1] == nums[r]) --r;
+                }
+                else if (sum > 0) --r;
+                else ++l;
+            }
+        }
+        return res;
+    }
+};
+#include <vector>
 #include <set>
 #include <unordered_set>
 #include <unordered_map>

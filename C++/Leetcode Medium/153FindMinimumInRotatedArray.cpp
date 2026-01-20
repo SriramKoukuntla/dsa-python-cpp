af55952759620aa8be2349321ffd428c32509109
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2026-01-20 04:55:12 -0600

    153FindMinimumInRotatedArrayCpp

--- C++/Leetcode Medium/153FindMinimumInRotatedArray.cpp
+++ C++/Leetcode Medium/153FindMinimumInRotatedArray.cpp
@@ -1,4 +1,20 @@
 #include <vector>
+using namespace std;
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int l = 0;
+        int r = nums.size()-1;
+        while (l < r) {
+            int m = l + ((r-l)/2);
+            if (nums[m] < nums[r]) r = m;
+            else l = m+1;
+
+        }
+        return nums[l];
+    }
+};
+#include <vector>
 #include <algorithm>
 using namespace std;
 class Solution {

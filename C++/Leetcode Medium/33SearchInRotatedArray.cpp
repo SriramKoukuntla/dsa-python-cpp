281870d7d4e52ab52e1fb5321e4b4513cc0e41a6
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2026-01-21 02:52:52 -0600

    33SearchInRotatedArrayCpp

--- C++/Leetcode Medium/33SearchInRotatedArray.cpp
+++ C++/Leetcode Medium/33SearchInRotatedArray.cpp
@@ -1,5 +1,28 @@
 #include <vector>
 using namespace std;
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l = 0; 
+        int r = nums.size()-1;
+        while (l <= r) {
+            int m = l + ((r-l)/2);
+            if (nums[m] == target) return m;
+            if (nums[m] < nums[r]) {
+                if (nums[m] < target && target <= nums[r]) l = m+1;
+                else r = m-1;
+            }
+            else {
+                if (nums[l] <= target && target < nums[m]) r = m-1;
+                else l = m+1;
+            }
+        }
+        return -1;
+    }
+};
+
+#include <vector>
+using namespace std;
 
 class Solution {
 public:

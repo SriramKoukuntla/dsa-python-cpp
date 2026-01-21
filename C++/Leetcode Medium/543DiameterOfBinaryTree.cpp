cadcf98ea920517c26382a156f3739b124fd5af7
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2026-01-21 04:22:07 -0600

    543DiameterOfBinaryTreeCpp

--- C++/Leetcode Medium/543DiameterOfBinaryTree.cpp
+++ C++/Leetcode Medium/543DiameterOfBinaryTree.cpp
@@ -9,6 +9,37 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+#include <utility>
+using namespace std;
+class Solution {
+public:
+    int diameterOfBinaryTree(TreeNode* root) {
+        int res = 0;
+        helper(root, res);
+        return res;
+    }
+    int helper(TreeNode* node, int& res) {
+        if (!(node->left) && !(node->right)) return 0;
+        int leftDepth = -1;
+        if (node->left) leftDepth = helper(node->left, res);
+        int rightDepth = -1;
+        if (node->right) rightDepth = helper(node->right, res);
+        res = max(res, leftDepth + rightDepth + 2);
+        return max(leftDepth, rightDepth) + 1;
+    }
+};
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 #include <algorithm>
 class Solution {
 public:

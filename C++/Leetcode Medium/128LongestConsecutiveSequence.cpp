10decc08d9c16c5cdc9547fa705214c025f1cead
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2026-01-22 01:34:15 -0600

    128LongestConsecutiveSequenceRedid

--- C++/Leetcode Medium/128LongestConsecutiveSequence.cpp
+++ C++/Leetcode Medium/128LongestConsecutiveSequence.cpp
@@ -2,6 +2,25 @@ class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
         unordered_set<int> set;
+        for(int i : nums) set.insert(i);
+        int res = 0;
+        for (int i : set){
+            if (set.find(i-1) != set.end()) continue;
+            int curr = i;
+            int temp = 0;
+            while (set.find(curr) != set.end()) {
+                ++curr;
+                ++temp;
+            }
+            res = max(res, temp);
+        }
+        return res;
+    }
+};
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int> set;
         for (int i : nums) set.insert(i);
         int res = 0;
         for (int i : set) {

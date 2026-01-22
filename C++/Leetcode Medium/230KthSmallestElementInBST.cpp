1145b77f7175e2cbb3016ee79387201b925c294c
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2026-01-22 01:21:26 -0600

    230KthSmallestElementInBSTRedid

--- C++/Leetcode Medium/230KthSmallestElementInBST.cpp
+++ C++/Leetcode Medium/230KthSmallestElementInBST.cpp
@@ -12,6 +12,35 @@
 class Solution {
 public:
     int kthSmallest(TreeNode* root, int k) {
+        int res = -1;
+        helper(res, root, k);
+        return res;
+    }
+    void helper(int& res, TreeNode* node, int& k) {
+        if (!node) return;
+        helper(res, node->left, k);
+        if (--k == 0) {
+            res = node->val;
+            return;
+        }
+        helper(res, node->right, k);
+    }
+};
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int kthSmallest(TreeNode* root, int k) {
         int n = 0;
         vector<TreeNode*> stack;
         TreeNode* curr = root;

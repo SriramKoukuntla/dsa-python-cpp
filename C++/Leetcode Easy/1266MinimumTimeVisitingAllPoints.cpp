a120eb7e660fa3a2bc246d56ebc111a95523b1db
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2026-01-26 10:51:31 -0600

    1266MinimumTimeVisitingAllPointsCpp

---
+++ C++/Leetcode Easy/1266MinimumTimeVisitingAllPoints.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int minTimeToVisitAllPoints(vector<vector<int>>& points) {
+        int res = 0;
+        vector<int> curr = points[0];
+        for (int i = 1; i < points.size(); ++i) {
+            vector<int> dst = points[i];
+            while (curr[0] != dst[0] || curr[1] != dst[1]) {
+                if (curr[0] < dst[0]) ++curr[0];
+                if (curr[0] > dst[0]) --curr[0];
+                if (curr[1] < dst[1]) ++curr[1];
+                if (curr[1] > dst[1]) --curr[1];
+                ++res;
+            }
+        }
+        return res;
+    }
+};

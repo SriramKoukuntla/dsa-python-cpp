b866c5c7a9a48bdedd406f665cfaf5c0521c4f0f
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2026-01-26 09:48:18 -0600

    268MissingNumberCpp

--- C++/Leetcode Easy/268MissingNumber.cpp
+++ C++/Leetcode Easy/268MissingNumber.cpp
@@ -1,3 +1,15 @@
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        vector<bool> res(nums.size()+1, false);
+        for (int n : nums) res[n] = true;
+        for (int i = 0; i < res.size(); ++i) if (!res[i]) return i;
+        return -1;
+    }
+};
+
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {

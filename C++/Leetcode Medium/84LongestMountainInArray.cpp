d28302a9360416185d975c8d89e4384ce9b5460c
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2026-01-28 16:06:39 -0600

    84LongestMountainInArrayCpp

--- C++/Leetcode Medium/84LongestMountainInArray.cpp
+++ C++/Leetcode Medium/84LongestMountainInArray.cpp
@@ -4,6 +4,28 @@ class Solution {
 public:
     int longestMountain(vector<int>& arr) {
         int res = 0;
+        for (int i = 1; i < arr.size()-1; ++i) {
+            int l = i-1;
+            int r = i+1;
+            if (!(arr[l] < arr[l+1] && arr[r-1] > arr[r])) continue;
+
+            while ((l >= 0  && arr[l] < arr[l+1]) || (r < arr.size() && arr[r-1] > arr[r])) {
+                if (l >= 0  && arr[l] < arr[l+1]) --l;
+                if (r < arr.size() && arr[r-1] > arr[r]) ++r;
+            }
+            res = max(res, r-l+1-2);
+        }   
+        return res < 3 ? 0 : res;
+    }
+};
+
+
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int longestMountain(vector<int>& arr) {
+        int res = 0;
         for (int i = 0; i < arr.size(); ++i) {
             int l = i-1;
             int r = i+1;

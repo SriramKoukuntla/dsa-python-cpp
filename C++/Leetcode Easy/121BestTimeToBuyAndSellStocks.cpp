83024c5c5fc23be9d5a6c99d536624e257f6cd1c
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2026-01-28 14:34:32 -0600

    121BestTimeToBuyAndSellStocksRedid

--- C++/Leetcode Easy/121BestTimeToBuyAndSellStocks.cpp
+++ C++/Leetcode Easy/121BestTimeToBuyAndSellStocks.cpp
@@ -3,6 +3,21 @@ using namespace std;
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
+        int res = 0;
+        int l = 0;
+        for (int r = 0; r < prices.size(); ++r) {
+            res = max(res, prices[r]-prices[l]);
+            if (prices[r] < prices[l]) l = r;
+        }
+        return res;
+    }
+};
+
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
         int maxP = 0;
         int left = prices[0];
         for (int i = 0; i < prices.size(); ++i){

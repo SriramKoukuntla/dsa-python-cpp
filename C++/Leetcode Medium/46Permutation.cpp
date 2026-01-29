5d5f087f589d6f24c2673df91fb20f041326e165
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2026-01-29 13:18:06 -0600

    46PermutationCpp

--- C++/Leetcode Medium/46Permutation.cpp
+++ C++/Leetcode Medium/46Permutation.cpp
@@ -1,3 +1,31 @@
+#include <vector>
+#include <unordered_set>
+using namespace std;
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<bool> used(nums.size(), false);
+        vector<int> temp;
+        vector<vector<int>> res;
+        helper(used, used.size(), temp, res, nums);
+        return res;
+    }
+
+    void helper(vector<bool>& used, int left, vector<int>& temp, vector<vector<int>>& res, vector<int>& nums) {
+        if (left == 0) res.push_back(temp);
+        for (int i = 0; i < nums.size(); ++i) {
+            if (used[i]) continue;
+            temp.push_back(nums[i]);
+            used[i] = true;
+            --left;
+            helper(used, left, temp, res, nums);
+            temp.pop_back();
+            used[i] = false;
+            ++left;
+        }
+    }
+};
+
 //Backtracking Solution
 #include <vector>
 using namespace std;

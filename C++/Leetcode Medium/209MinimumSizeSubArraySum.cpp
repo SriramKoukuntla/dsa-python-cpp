827bd341e24c36fa5762de7e2682d04b4f612631
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2026-01-28 18:35:49 -0600

    209MinimumSizeSubArraySumCpp

---
+++ C++/Leetcode Medium/209MinimumSizeSubArraySum.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int l = 0;
+        int sum = 0;
+        int res = nums.size()+1;
+        for (int r = 0; r < nums.size(); ++r) {
+            sum += nums[r];
+            while (sum >= target) {
+                res = min(res, r-l+1);
+                sum -= nums[l++];
+            }
+        }
+        return res == nums.size()+1 ? 0 : res;
+    }
+};

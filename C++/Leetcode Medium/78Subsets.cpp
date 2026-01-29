9b173c02a2e79d00455bee601a5304638c035b17
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2026-01-29 07:26:38 -0600

    78SubsetsRedid

--- C++/Leetcode Medium/78Subsets.cpp
+++ C++/Leetcode Medium/78Subsets.cpp
@@ -1,3 +1,20 @@
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> res = {{}};
+        for(int i = 0; i < nums.size(); ++i) {
+            int n = res.size();
+            for(int j = 0; j < n; ++j) {
+                res.push_back(res[j]);
+                res.back().push_back(nums[i]);
+            }
+        }
+        return res;
+    }
+};
+
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {

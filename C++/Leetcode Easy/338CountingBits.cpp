abb739363777496b34003f04f3a7b01a945e032e
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2026-01-29 05:47:55 -0600

    338CountingBitsCpp

--- C++/Leetcode Easy/338CountingBits.cpp
+++ C++/Leetcode Easy/338CountingBits.cpp
@@ -3,6 +3,24 @@ using namespace std;
 class Solution {
 public:
     vector<int> countBits(int n) {
+        if (n == 0) return {0};
+        else if (n == 1) return {0, 1};
+        vector<int> res(n+1, 0);
+        res[0] = 0; res[1] = 1;
+        for (int curr = 2; curr <= n; ++curr) {
+            int temp = curr % 2;
+            temp += res[curr/2];
+            res[curr] = temp;
+        }
+        return res;
+    }
+};
+
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    vector<int> countBits(int n) {
         vector<int> res(n+1, 0);
         for (int i = 1; i < n+1; ++i){
             res[i] = res[i >> 1] + (i & 1);

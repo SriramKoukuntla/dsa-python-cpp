e03ad529cabf1c29135c2dbc70f9eb09d3a3c6df
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2026-01-28 20:00:40 -0600

    322CoinChangeCpp

--- C++/Leetcode Medium/322CoinChange.cpp
+++ C++/Leetcode Medium/322CoinChange.cpp
@@ -3,6 +3,21 @@ using namespace std;
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
+        vector<int> mem(amount+1, amount+1);
+        mem[0] = 0;
+        for (int i = 1; i < amount+1; ++i) {
+            for (int coin : coins) {
+                if (coin <= i) mem[i] = min(mem[i], mem[i-coin]+1);
+            }
+        }
+        return mem[amount] == amount+1 ? -1 : mem[amount];
+    }
+};
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
         sort(coins.begin(), coins.end());
         vector<int> mem(amount+1, -1);
         mem[0] = 0;

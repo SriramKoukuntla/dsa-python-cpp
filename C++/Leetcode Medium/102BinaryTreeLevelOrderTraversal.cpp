e721ac84ea91b3bc782d031ae57f66a46ec74791
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2026-01-30 08:01:48 -0600

    102BinaryTreeLevelOrderTraversalcpp

--- C++/Leetcode Medium/102BinaryTreeLevelOrderTraversal.cpp
+++ C++/Leetcode Medium/102BinaryTreeLevelOrderTraversal.cpp
@@ -10,6 +10,41 @@
  * };
  */
 #include <list>
+using namespace std;
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        if (!root) return {};
+        vector<vector<int>> res;
+        list<TreeNode*> queue = {root};
+        while (!queue.empty()) {
+            int n = queue.size();
+            vector<int> temp;
+            for (int i = 0; i < n; ++i) {
+                TreeNode* curr = queue.front();
+                queue.pop_front();
+                temp.push_back(curr->val);
+                if (curr->left) queue.push_back(curr->left);
+                if (curr->right) queue.push_back(curr->right);
+            }
+            res.push_back(temp);
+        }
+        return res;
+    }
+};
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+#include <list>
 #include <vector>
 using namespace std;
 class Solution {

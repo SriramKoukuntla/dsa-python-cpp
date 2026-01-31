c4eed379822fc4c9c1616e899c45f8735cac07c9
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2026-01-31 06:57:55 -0600

    DijkstrasAlgorithm.cpp

---
+++ C++/CoreAlgorithms/DjistrasAlgorithm.cpp
@@ -0,0 +1,44 @@
+#include <vector>
+#include <unordered_map>
+#include <queue>
+#include <utility>
+#include <functional>
+using namespace std;
+
+
+//Input: Vector of edges: {node1, node2, weight}
+//Output: Map of shortest path from node1 to node2 (key = node dst, val = shortest path length)
+unordered_map<int, int> djkstraAlgorithm(vector<vector<int>> edges, int starting) {
+    //Create Adjacency Map, res map, and priority queue
+    unordered_map<int, vector<vector<int>>> map;
+    for (vector<int> edge : edges) map[edge[0]].push_back({edge[1], edge[2]});
+    unordered_map<int, int> res;
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    pq.push({0, starting});
+
+    //Core algorithm
+    while (!pq.empty()) {
+        //Get Current Node in pq
+        int currDist = pq.top().first;
+        int currNode = pq.top().second;
+        pq.pop();
+        
+        //Finalize Node
+        if (res.find(currNode) != res.end()) continue;
+        res[currNode] = currDist;
+        
+        //Relaxation of current node
+        for (vector<int> edge : map[currNode]) {
+            int n = edge[0];
+            int w = edge[1];
+
+            if (res.find(n) != res.end()) continue;
+            pq.push({currDist + w, n});
+        }
+    }
+    
+    return res;
+}
+
+
+

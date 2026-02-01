c6e0cebb73735bab44d16ceab3d4d45d0d62f95c
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2026-02-01 12:03:54 -0500

    KahnsAlgorithmCpp

--- C++/CoreAlgorithms/KahnsAlgorithm.cpp
+++ C++/CoreAlgorithms/KahnsAlgorithm.cpp
@@ -1,7 +1,8 @@
-//Kahn's algorithm is used mainly for topologically sorting a directed graph
+//Kahn's algorithm is used mainly for topologically sorting a directed graph + detecting for cycles
 
 #include <vector>
 #include <list>
+#include <unordered_set>
 using namespace std;
 vector<int> KahnsAlgorithm (vector<vector<int>> directedEdges, int n) {
     //directedEdges [[0, 1], [2, 0], [1, 2]] means 0 points to 1. 2 points to 0. 1 points to 2.
@@ -15,6 +16,7 @@ vector<int> KahnsAlgorithm (vector<vector<int>> directedEdges, int n) {
 
     //Adding out starting nodes to queue
     list<int> queue;
+    unordered_set<int> visited;
     for (int i = 0; i < indegree.size(); ++i) if (indegree[i] == 0) queue.push_back(i);
     vector<int> res;
 
@@ -22,10 +24,10 @@ vector<int> KahnsAlgorithm (vector<vector<int>> directedEdges, int n) {
     while (!queue.empty()) {
         int curr = queue.front();
         queue.pop_front();
+        if (visited.find(curr) != visited.end()) return {}; //Checking for cycle
+        visited.insert(curr);
         res.push_back(curr);
         for (int outgoingNode : outgoingEdges[curr]) if (--indegree[outgoingNode] == 0) queue.push_back(outgoingNode);
     }
-    
-    //Checck for cycle before returning function
-    return (res.size() != n) ? vector<int>() : res;
+    return (res.size() == n) ? res : vector<int>();
 }

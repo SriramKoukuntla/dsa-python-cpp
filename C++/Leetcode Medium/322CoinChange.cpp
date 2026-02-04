3a3eeaede74cd3062a99f2e864b5ed0d355a7cdf
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2026-02-03 19:58:08 -0500

    322CoinChangeRedid

--- C++/Leetcode Medium/322CoinChange.cpp
+++ C++/Leetcode Medium/322CoinChange.cpp
@@ -1,3 +1,33 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        sort(coins.begin(), coins.end(), greater<int>());
+        vector<vector<int>> mem(coins.size(), vector<int>(amount+1, -2));
+        return helper(coins, amount, 0, 0, mem);
+    }
+
+    int helper(vector<int>& coins, int amount, int i, int currSum, vector<vector<int>>& mem) {
+        if (currSum == amount) return 0;
+        if (i >= coins.size()) return -1;    
+        if (currSum > amount) return -1;
+        if (mem[i][currSum] != -2) return mem[i][currSum];
+
+
+        int stay = helper(coins, amount, i, currSum + coins[i], mem);
+        if (stay != -1) ++stay;
+        int next = helper(coins, amount, i+1, currSum, mem);
+        
+        int res = 0;
+        if (stay == -1 && next == -1) res = -1;
+        else if (stay == -1) res = next;
+        else if (next == -1) res = stay;
+        else res = min(stay, next);
+        mem[i][currSum] = res;
+        return res;
+    }
+};
+
+
 #include <vector>
 using namespace std;
 class Solution {

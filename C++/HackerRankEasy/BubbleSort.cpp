e6b3da9f431366e6144438373a47fb2dc0955c07
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2026-02-06 13:38:56 -0500

    BubbleSortcpp

---
+++ C++/HackerRankEasy/BubbleSort.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+void countSwaps(vector<int> a) {
+    int numSwaps = 0;
+    for (int i = 0; i < a.size(); ++i) {
+        for (int j = 0; j < a.size()-1; ++j) {
+            if (a[j] > a[j+1]) {
+                swap(a[j], a[j+1]);
+                ++numSwaps;
+            }
+        }        
+    }
+    cout << "Array is sorted in " << numSwaps << " swaps." << endl;
+    cout << "First Element: " << a[0] << endl;
+    cout << "Last Element: " << a[a.size()-1] << endl;
+}

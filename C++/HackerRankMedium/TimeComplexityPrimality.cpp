9cedf2a3ce2bba7d5d07cc29a8ce3b8a2a9094b3
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2026-02-11 12:23:52 -0500

    TimeComplexityPrimalityCpp

---
+++ C++/HackerRankMedium/TimeComplexityPrimality.cpp
@@ -0,0 +1,7 @@
+#include <string>
+using namespace std;
+string primality(int n) {
+    if (n == 1) return "Not prime";
+    for (int i = 2; i*i <= n; ++i) if (n % i == 0) return "Not prime";
+    return "Prime";
+}

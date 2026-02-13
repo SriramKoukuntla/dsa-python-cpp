c95c28e51db559839c1e8729d352b852e3c2df68
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2026-02-13 14:41:37 -0500

    1235MaximumProfitInJobSchedulingCpp

--- C++/Leetcode Hard/1235MaximumProfitInJobScheduling.cpp
+++ C++/Leetcode Hard/1235MaximumProfitInJobScheduling.cpp
@@ -1,6 +1,48 @@
 #include <vector>
 #include <unordered_map>
 #include <unordered_set>
+using namespace std;
+class Solution {
+public:
+    int helper(unordered_map<int, vector<pair<int, int>>>& timing, int time, int lastTime, 
+                unordered_map<int, int>& mem, unordered_map<int, int>& nextTime) {
+        if (time == lastTime) return 0;
+        if (mem.find(time) != mem.end()) return mem[time];
+        int res = 0;
+        for (auto [endTime, profit] : timing[time]) res = max(res, profit + helper(timing, endTime, lastTime, mem, nextTime));
+        if (nextTime[time] != -1) res = max(res, helper(timing, nextTime[time], lastTime, mem, nextTime));
+        mem[time] = res;
+        return res;
+    }
+
+    int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
+        unordered_map<int, vector<pair<int, int>>> timings; //[key = starTime] [val = list of pairs [endTime, profit]]
+        int lastTime = 0;
+        unordered_set<int> validTimes;
+        for (int i = 0; i < startTime.size(); ++i) {
+            timings[startTime[i]-1].push_back({endTime[i]-1, profit[i]}); //0 indexed
+            lastTime = max(lastTime, endTime[i]-1);
+            validTimes.insert(startTime[i]-1);
+            validTimes.insert(endTime[i]-1);
+        }
+
+        vector<int> sortedValidTimes(validTimes.begin(), validTimes.end());
+        sort(sortedValidTimes.begin(), sortedValidTimes.end());
+
+
+        unordered_map<int, int> nextTime; //validTime1 -> validTime2 (if last valid time, then -1);
+        for (int i = 0; i < sortedValidTimes.size()-1; ++i) nextTime[sortedValidTimes[i]] = sortedValidTimes[i+1];
+        nextTime[sortedValidTimes[sortedValidTimes.size()-1]] = -1;
+
+        unordered_map<int, int> mem;
+        return helper(timings, sortedValidTimes[0], lastTime, mem, nextTime);
+    }
+};
+
+
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
 #include <algorithm>
 using namespace std;
 class Solution {

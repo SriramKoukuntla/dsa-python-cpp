f4022204d80ab5ebdf47994d7aeefbabe84d2d84
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2026-02-13 16:10:56 -0500

    39CombinationSumRedid

--- C++/Leetcode Medium/39CombinationSum.cpp
+++ C++/Leetcode Medium/39CombinationSum.cpp
@@ -1,3 +1,26 @@
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    void helper(vector<int>& candidates, int target, int index, int currSum, vector<int>& combination,
+                vector<vector<int>>& res) {
+        if (currSum == target) {res.push_back(combination); return;}
+        else if (currSum > target || index >= candidates.size()) return;
+        combination.push_back(candidates[index]);
+        helper(candidates, target, index, currSum + candidates[index], combination, res); //Taking repicking same element
+        combination.pop_back();
+        helper(candidates, target, index+1, currSum, combination, res); //Skipping current element taking next element
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> res;
+        vector<int> combination;
+        helper(candidates, target, 0, 0, combination, res);
+        return res;
+    }
+};
+
+
 #include <algorithm>
 class Solution {
 public:

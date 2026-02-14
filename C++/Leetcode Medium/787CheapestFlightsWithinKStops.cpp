ec50132ab055835335e881e95719e2af091835dc
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2026-02-13 20:36:33 -0500

    787CheapestFlightsWIthinKStopsCpp

--- C++/Leetcode Medium/787CheapestFlightsWithinKStops.cpp
+++ C++/Leetcode Medium/787CheapestFlightsWithinKStops.cpp
@@ -1,3 +1,30 @@
+#include <vector>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        unordered_map<int, vector<pair<int, int>>> adjList; //[from, list[to, price]]
+        for (vector<int>& flight : flights) adjList[flight[0]].push_back({flight[1], flight[2]});
+
+        vector<int> knownDistances(n, INT_MAX);
+        knownDistances[src] = 0;
+        for (int i = 0; i < k+1; ++i) {
+            vector<int> tempKnownDistances = knownDistances;
+            for (int j = 0; j < knownDistances.size(); ++j) {
+                if (knownDistances[j] == INT_MAX) continue;
+                for (auto [to, price] : adjList[j]) {
+                    tempKnownDistances[to] = min(tempKnownDistances[to], knownDistances[j] + price);
+                }
+            }
+            knownDistances = tempKnownDistances;
+        }
+        return knownDistances[dst] == INT_MAX ? -1 : knownDistances[dst];
+    }
+};
+
+
 #include <deque>
 #include <vector>
 #include <unordered_map>

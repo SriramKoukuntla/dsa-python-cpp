0a5ae207505bc8d24ca568b6b0a6d668ddb91b2f
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2026-02-15 23:36:40 -0500

    1TwoSumCpp

--- C++/Leetcode Easy/1TwoSum.cpp
+++ C++/Leetcode Easy/1TwoSum.cpp
@@ -6,6 +6,21 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int, int> map;
         for (int i = 0; i < nums.size(); ++i) {
+            if (map.find(target-nums[i]) != map.end()) return {i, map[target-nums[i]]};
+            map[nums[i]] = i;
+        }
+        return {-1, -1};
+    }
+};
+
+#include <vector>
+#include <unordered_map>
+using namespace std;
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> map;
+        for (int i = 0; i < nums.size(); ++i) {
             if (map.find(target-nums[i]) != map.end()) {
                 int j = map[target-nums[i]];
                 return {i, j};

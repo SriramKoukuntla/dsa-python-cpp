28d02ada4ed11e1633115ef2a3141ccf85e5cb5c
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2026-02-18 17:59:22 -0500

    136SingleNumberCppRedid

--- C++/Leetcode Easy/136SingleNumber.cpp
+++ C++/Leetcode Easy/136SingleNumber.cpp
@@ -1,3 +1,12 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int res = 0;
+        for (int num : nums) res ^= num;
+        return res;
+    }
+};
+
 #include <vector>
 using namespace std;
 class Solution {

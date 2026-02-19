d797cc031dfcae193a9c88634d81508cdb6e5a26
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2026-02-19 08:31:28 -0500

    UnionFindAlgorithmCpp

--- C++/CoreAlgorithms/UnionFindAlgorithm.cpp
+++ C++/CoreAlgorithms/UnionFindAlgorithm.cpp
@@ -1,28 +1,18 @@
-//For finding number of connected components
 #include <vector>
 using namespace std;
-int UnionFindAlgorithm(vector<vector<int>> edges, int n) {
-    vector<int> parents;
-    for (int i = 0; i < n; ++i) parents.push_back(i);
-    vector<int> size(n, 1);
-
-    int res = n;
-    for (vector<int> edge : edges) {
-        int p1 = find(parents, edge[0]);
-        int p2 = find(parents, edge[1]);
-        if (p1 == p2) continue;
-        if (size[p2] > size[p1]) swap(p1, p2);
-        parents[p2] = p1;
-        size[p1] += size[p2];
-        --res;
-    }
-    return res;
+//For finding number of connected components
+int find(vector<int>& parents, vector<int>& size, int node) {
+    return (parents[node] == node) ? node : parents[node] = find(parents, size, parents[node]);
 }
 
-int find(vector<int>& parent, int node) {
-    while (node != parent[node]) {
-        parent[node] = parent[parent[node]];
-        node = parent[node];
-    }
-    return node;
+bool unionOp(vector<int>& parents, vector<int>& size, int node1, int node2) {
+    int root1 = find(parents, size, node1);
+    int root2 = find(parents, size, node2);
+    if (root1 == root2) return false;
+    
+    if (size[root2] > size[root1]) swap(root1, root2);
+    size[root1] += size[root2];
+    parents[root2] = root1;
+
+    return true;
 }

c09382ad6c468f3e927869876c3fc121a76646f7
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2026-02-19 10:34:04 -0500

    251Flatten2DVectorCppRedid

--- C++/Leetcode Medium/251Flatten2DVector.cpp
+++ C++/Leetcode Medium/251Flatten2DVector.cpp
@@ -1,3 +1,45 @@
+class Vector2D {
+public:
+    vector<vector<int>> v;
+    int row;
+    int col;
+    void advance() {
+        while (row < v.size() && (v[row].size() == 0 || col == v[row].size())) {
+            ++row;
+            col = 0;
+        }
+    }
+
+    Vector2D(vector<vector<int>>& vec) {
+        v = vec;
+        row = 0;
+        col = 0;
+        advance();
+    }
+    
+    int next() {
+        advance();
+        int val = v[row][col];
+        ++col;
+        advance();
+        return val;
+    }
+    
+    bool hasNext() {
+        advance();
+        return (row < v.size());
+        
+    }
+};
+
+/**
+ * Your Vector2D object will be instantiated and called as such:
+ * Vector2D* obj = new Vector2D(vec);
+ * int param_1 = obj->next();
+ * bool param_2 = obj->hasNext();
+ */
+
+ 
 #include <vector>
 using namespace std;
 class Vector2D {

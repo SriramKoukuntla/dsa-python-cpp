5431f791511537653feb6b659a9f02d8f3834714
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2026-02-24 18:25:57 -0500

    200NumberOfIslandsCppRedid

--- C++/Leetcode Medium/200NumberOfIslands.cpp
+++ C++/Leetcode Medium/200NumberOfIslands.cpp
@@ -1,5 +1,28 @@
 #include <vector>
-using namesapce std;
+using namespace std;
+class Solution {
+public:
+    bool helper(vector<vector<char>>& grid, int i, int j) {
+        if (i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || grid[i][j] == '0') return false;
+        grid[i][j] = '0';
+        helper(grid, i-1, j);
+        helper(grid, i+1, j);
+        helper(grid, i, j-1);
+        helper(grid, i, j+1);
+        return true;
+    }
+
+    int numIslands(vector<vector<char>>& grid) {
+        int res = 0;
+        for (int i = 0; i < grid.size(); ++i) {
+            for (int j = 0; j < grid[0].size(); ++j) {
+                if (helper(grid, i, j)) ++res;
+            }
+        }
+        return res;
+    }
+};
+
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {

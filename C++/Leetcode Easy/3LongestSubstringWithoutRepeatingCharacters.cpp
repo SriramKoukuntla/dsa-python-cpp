55d8968f04a88455c8b170bf340a1b256a7243df
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2026-02-24 17:08:12 -0500

    3LongestSubstringWithoutRepeatingCharactersRedid

--- C++/Leetcode Easy/3LongestSubstringWithoutRepeatingCharacters.cpp
+++ C++/Leetcode Easy/3LongestSubstringWithoutRepeatingCharacters.cpp
@@ -1,3 +1,20 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_set<char> set;
+        int res = 0;
+        int l = 0;
+        for (int r = 0; r < s.size(); ++r) {
+            while (set.find(s[r]) != set.end()) {
+                set.erase(s[l++]);
+            }
+            set.insert(s[r]);
+            res = max(res, (int)set.size());
+        }
+        return res;
+    }
+};
+
 #include <string>
 #include <algorithm>
 #include <unordered_set>

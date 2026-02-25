1f5419c9714caeae1016b6bd48c1fe5300ae62cd
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2026-02-25 16:16:11 -0500

    169MajorityElementCpp

---
+++ C++/Leetcode Easy/169MajorityElement.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        vector<pair<int, int>> bitCounting(32, {0, 0}); //key = what bit, value freq of 0 & 1
+        for (int i = 0; i < nums.size(); ++i) {
+            int currNum = nums[i];
+            long mask = 1;
+            for (int j = 0; j < 32; ++j) {
+                if (currNum & mask) ++ bitCounting[j].second;
+                else ++bitCounting[j].first;
+                mask <<= 1;
+            }
+        }
+
+        int res = 0;
+        for (int i = 0; i < 32; ++i) {
+            if (bitCounting[i].first > bitCounting[i].second) continue;
+            int insertBit = 1 << i;
+            res = res | insertBit;
+        }
+
+        return res;
+    }
+};

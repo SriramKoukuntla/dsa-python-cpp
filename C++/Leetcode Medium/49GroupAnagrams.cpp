0d4a2bc999e78b0868d5927775926ba9b272f991
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2026-02-24 21:49:08 -0500

    49GroupAnagramsRedidCpp

--- C++/Leetcode Medium/49GroupAnagrams.cpp
+++ C++/Leetcode Medium/49GroupAnagrams.cpp
@@ -1,5 +1,26 @@
 #include <unordered_map>
 #include <string>
+using namespace std;
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> map;
+        for (int i = 0; i < strs.size(); ++i) {
+            vector<int> alphaFreq(26, 0);
+            for (int j = 0; j < strs[i].size(); ++j) ++alphaFreq[strs[i][j]-'a'];
+            string currHash = "";
+            for (int freq : alphaFreq) currHash += to_string(freq) + ",";
+            map[currHash].push_back(strs[i]);
+        }
+
+        vector<vector<string>> res;
+        for (auto& [sortedString, listOfStrings] : map) {
+            res.push_back(listOfStrings);
+        }
+        return res;
+    }
+};
+
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {

15b9b3a5f5800785b5ccfc00547f69648b929320
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2026-02-25 20:26:39 -0500

    11ContainerWithMostWaterRedid

--- C++/Leetcode Medium/11ContainerWithMostWater.cpp
+++ C++/Leetcode Medium/11ContainerWithMostWater.cpp
@@ -1,3 +1,24 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int l = 0;
+        int r = height.size()-1;
+
+        int res = 0;
+        while (l < r) {
+            int leftHeight = height[l];
+            int rightHeight = height[r];
+            int currVolume = min(leftHeight, rightHeight) * (r-l);
+            res = max(res, currVolume);
+
+            if (leftHeight < rightHeight) ++l;
+            else --r;
+        }
+
+        return res;
+    }
+};
+
 #include <vector>
 #include <algorithm>
 using namespace std;

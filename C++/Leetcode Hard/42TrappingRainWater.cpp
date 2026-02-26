0213a32915e5b5995fcedc2ae604270fa883d974
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2026-02-26 14:49:46 -0500

    42TrappingRainWaterRedidCpp

--- C++/Leetcode Hard/42TrappingRainWater.cpp
+++ C++/Leetcode Hard/42TrappingRainWater.cpp
@@ -1,3 +1,33 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int left = 0;
+        int right = height.size()-1;
+        int maxLeftHeight = height[left];
+        int maxRightHeight = height[right];
+
+        int res = 0;
+        while (left < right-1) {
+            int minBoundaryHeight = min(maxLeftHeight, maxRightHeight);
+            int leftRightCurr = height[left+1];
+            int rightLeftCurr = height[right-1];
+            int leftCurr = height[left];
+            int rightCurr = height[right];
+            if (leftCurr <= rightCurr) {
+                maxLeftHeight = max(maxLeftHeight, leftRightCurr);
+                res += max(0, minBoundaryHeight-leftRightCurr);
+                ++left;
+            }
+            else {
+                maxRightHeight = max(maxRightHeight, rightLeftCurr);
+                res += max(0, minBoundaryHeight-rightLeftCurr);
+                --right;
+            }
+        }
+        return res;
+    }
+};
+
 #include <vector>
 using namespace std;
 class Solution {

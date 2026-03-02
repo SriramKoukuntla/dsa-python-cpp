d64aeb6a183442f64645ce65bab9043d2df2599f
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2026-03-02 00:13:47 -0500

    217 ContainsDuplicatesRedidCpp

--- C++/Leetcode Easy/217ContainsDuplicates.cpp
+++ C++/Leetcode Easy/217ContainsDuplicates.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> seen;
+        for (int num : nums) {
+            if (seen.find(num) != seen.end()) return true;
+            seen.insert(num);
+        }
+        return false;
+    }
+};
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
         unordered_set<int> set;
         for (int i = 0; i < nums.size(); ++i) {
             if (set.find(nums[i]) != set.end()) return true;

12daf7046cfe1313be6d1be67fc699d8b8112b5c
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2026-03-02 18:31:31 -0500

    23MergeKSortedListsRedidCpp

--- C++/Leetcode Hard/23MergeKSortedLists.cpp
+++ C++/Leetcode Hard/23MergeKSortedLists.cpp
@@ -1,5 +1,38 @@
 /**
  * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<pair<int, ListNode*>, vector<pair<int, ListNode*>>, greater<pair<int, ListNode*>>> pq;
+        for (ListNode* head : lists) if (head) pq.push({head->val, head});
+        ListNode* dummy = new ListNode();
+        ListNode* curr = dummy;
+        while (!pq.empty()) {
+            auto [nodeVal, node] = pq.top();
+            pq.pop();
+            curr->next = node;
+            curr = curr->next;
+            ListNode* nextNode = node->next;
+            if (nextNode) pq.push({nextNode->val, nextNode});
+        }
+
+        ListNode* res = dummy->next;
+        delete dummy;
+        return res;
+    }
+};
+
+
+/**
+ * Definition for singly-linked list.
  */
 #include <vector>
 #include <queue>

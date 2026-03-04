e9358765dbcaeb92662d627a875fa282ef9279ec
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2026-03-04 16:59:30 -0500

    387FirstUniqueCharacterInAStringCpp

---
+++ C++/Leetcode Easy/387FirstUniqueCharacterInAString.cpp
@@ -0,0 +1,11 @@
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        vector<int> freq(26, 0);
+        for (char c : s) ++freq[c-'a'];
+        for (int i = 0; i < s.size(); ++i) if (freq[s[i]-'a'] < 2) return i;  
+        return -1;
+    }
+};

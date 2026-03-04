c12563c10b0f0922b9f9a542c401b4b1bc9d3344
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2026-03-04 16:19:18 -0500

    4MedianOfTwoSortedArrayRedidCpp

--- C++/Leetcode Hard/4MedianOfTwoSortedArrays.cpp
+++ C++/Leetcode Hard/4MedianOfTwoSortedArrays.cpp
@@ -1,5 +1,42 @@
 #include <vector>
+#include <iostream>
 using namespace std;
+
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        if (nums2.size() > nums1.size()) swap(nums1, nums2);
+        vector<int>& smaller = nums2;
+        vector<int>& bigger = nums1;
+
+        int total = smaller.size() + bigger.size();
+        int half = total/2;
+
+        int l = -1;
+        int r = smaller.size()-1;
+        while (l <= r) {
+            int m = l + (r-l)/2; //for smaller (index of the right most value of the left partition on this array)
+            int m2 = (half - (m+1))-1; //for bigger (index of the right most value of the left partition on this array)
+
+            int smallerLeft = (m < 0) ? INT_MIN : smaller[m];
+            int smallerRight = (m+1 == smaller.size()) ? INT_MAX : smaller[m+1];
+
+            int biggerLeft = (m2 < 0) ? INT_MIN : bigger[m2];
+            int biggerRight = (m2+1 == bigger.size()) ? INT_MAX : bigger[m2+1];
+
+            if (smallerLeft <= biggerRight && biggerLeft <= smallerRight) {
+                if (total % 2 == 1) return min(smallerRight, biggerRight);
+                else return (max(smallerLeft, biggerLeft) + min(smallerRight, biggerRight))/2.0;
+            }
+            else if (smallerLeft > biggerRight) r = m-1;
+            else l = m+1;
+        }
+        cout << "THIS SHOULD NOT PRINT" << endl;
+        return -1;
+    }
+};
+
+
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {

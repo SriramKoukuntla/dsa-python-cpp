88f1c675ad866e795ad8143fa5bc51f452a13c96
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2026-03-08 21:34:48 -0400

    20ValidParenthesisRedid

--- C++/Leetcode Easy/20ValidParenthesis.cpp
+++ C++/Leetcode Easy/20ValidParenthesis.cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
     bool isValid(string s) {
+        vector<char> stack; 
+        for (char c : s) {
+            if (c == '(' || c == '{' || c == '[') stack.push_back(c);
+            else {
+                if (stack.empty()) return false;
+                char currChar = stack.back();
+                stack.pop_back();
+                if (currChar == '(' && c != ')') return false;
+                else if (currChar == '[' && c != ']') return false;
+                else if (currChar == '{' && c != '}') return false;
+            }
+        }
+        return stack.empty();
+    }
+};
+
+class Solution {
+public:
+    bool isValid(string s) {
         vector<char> stack;
         for (char c : s) {
             if (c == '(' || c == '{' || c == '[') stack.push_back(c);

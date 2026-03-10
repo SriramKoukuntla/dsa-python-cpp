04bbd42b7f3a98b223a238bc9e806f4bca988814
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2026-03-10 18:55:30 -0400

    76MinimumWindowSubstringRedidCpp

--- C++/Leetcode Hard/76MinimumWindowSubstring.cpp
+++ C++/Leetcode Hard/76MinimumWindowSubstring.cpp
@@ -1,3 +1,39 @@
+class Solution {
+public:
+    //Checks if the window meets the letter frequency requirements of t
+    bool windowValid(unordered_map<char, int>& windowFreq, unordered_map<char, int>& t) {
+        for (auto [letter, freq] : t) if (windowFreq[letter] < freq) return false;
+        return true;
+    }
+
+    string minWindow(string s, string t) {
+        //Count frequency of letters in t
+        unordered_map<char, int> tFreq;
+        for (char c : t) ++tFreq[c];
+
+        //Sliding window algorithm
+        unordered_map<char, int> windowFreq;
+        int resLength = s.size();
+        int resStart = -1;
+        int l = 0;
+        for (int r = 0; r < s.size(); ++r) {
+            ++windowFreq[s[r]];
+
+            //Making window smaller whenever it's valid
+            while (windowValid(windowFreq, tFreq)) {
+                if (r-l+1 <= resLength) { //updating res for every valid window if smaller than previously seen
+                    resLength = r-l+1;
+                    resStart = l;
+                }
+                --windowFreq[s[l]];
+                ++l;
+            }
+        }
+        return (resStart == -1) ? "" : s.substr(resStart, resLength);
+    }
+};
+
+
 #include <unordered_map>
 using namespace std;
 class Solution {

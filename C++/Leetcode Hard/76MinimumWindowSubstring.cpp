334664f2ac051f8b7e7949011f9bc969011bef7f
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2026-03-10 19:19:04 -0400

    76MinimumWindowSubstringCpp

--- C++/Leetcode Hard/76MinimumWindowSubstring.cpp
+++ C++/Leetcode Hard/76MinimumWindowSubstring.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public:
-    //Checks if the window meets the letter frequency requirements of t
-    bool windowValid(unordered_map<char, int>& windowFreq, unordered_map<char, int>& t) {
-        for (auto [letter, freq] : t) if (windowFreq[letter] < freq) return false;
-        return true;
-    }
-
     string minWindow(string s, string t) {
         //Count frequency of letters in t
         unordered_map<char, int> tFreq;
         for (char c : t) ++tFreq[c];
+        int uniqueLettersT = tFreq.size();
 
         //Sliding window algorithm
         unordered_map<char, int> windowFreq;
+        int numUniqueLettersFitCondition = 0;
         int resLength = s.size();
         int resStart = -1;
         int l = 0;
         for (int r = 0; r < s.size(); ++r) {
-            ++windowFreq[s[r]];
+            if (++windowFreq[s[r]] == tFreq[s[r]]) ++numUniqueLettersFitCondition;
 
             //Making window smaller whenever it's valid
-            while (windowValid(windowFreq, tFreq)) {
-                if (r-l+1 <= resLength) { //updating res for every valid window if smaller than previously seen
-                    resLength = r-l+1;
-                    resStart = l;
-                }
-                --windowFreq[s[l]];
-                ++l;
+            while (numUniqueLettersFitCondition == uniqueLettersT) {
+                    if (r-l+1 <= resLength) { //updating res for every valid window if smaller than previously seen
+                        resLength = r-l+1;
+                        resStart = l;
+                    }
+                    ++l;
+                    if (--windowFreq[s[l-1]] < tFreq[s[l-1]]) --numUniqueLettersFitCondition;
             }
         }
+
         return (resStart == -1) ? "" : s.substr(resStart, resLength);
     }
 };

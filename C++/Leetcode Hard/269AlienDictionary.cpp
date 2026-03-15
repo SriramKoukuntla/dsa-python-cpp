68d4ace8cc6c139df2da5c0f8d18236b8ada20be
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2026-03-15 19:47:21 -0400

    269AlienDictionaryRedidCpp

--- C++/Leetcode Hard/269AlienDictionary.cpp
+++ C++/Leetcode Hard/269AlienDictionary.cpp
@@ -1,3 +1,65 @@
+class Solution {
+public:
+    string alienOrder(vector<string>& words) {
+        //Perform linear scan incase of early exit
+        for (int i = 0; i < words.size()-1; ++i) {
+            string currWord = words[i];
+            string nextWord = words[i+1];
+
+            int minLen = min(currWord.size(), nextWord.size());
+            if (currWord.substr(0, minLen) == nextWord.substr(0, minLen) && nextWord.size() < currWord.size()) return "";
+        }
+
+        //Create innodes and adjlist
+        unordered_map<char, int> indeg;
+        for (string& word : words) for (char c : word) indeg[c] = 0;
+        unordered_map<char, vector<char>> adjList;
+        for (int i = 0; i < words.size()-1; ++i) {
+            string& currString = words[i];
+            string& nextString = words[i+1];
+            for (int j = 0; j < (int)min(currString.size(), nextString.size()); ++j) {
+                if (currString[j] != nextString[j]) {
+                    ++indeg[nextString[j]];
+                    adjList[currString[j]].push_back(nextString[j]);
+                    break;
+                }
+            }
+        }
+
+        //Populate queue with letters with indegree of zero
+        string res = "";
+        deque<char> queue;
+        for (auto [letter, currIndegree] : indeg) {
+            if (currIndegree == 0) {
+                queue.push_back(letter);
+                res.push_back(letter);
+            }
+        }
+
+        //Core alg of khan's algorithm
+        while (!queue.empty()) {
+            char currLetter = queue.front();
+            queue.pop_front();
+
+            for (char nextLetter : adjList[currLetter]) {
+                if (--indeg[nextLetter] == 0) {
+                    queue.push_back(nextLetter);
+                    res.push_back(nextLetter);
+                }
+            }
+        }
+
+        //This will check for cycle
+        for (auto [letter, currIndegree] : indeg) if (currIndegree != 0) return "";
+        return res;
+    }
+};
+//Linear scan to see if the first (min(a.length, b.length) characters match && a.size < b.size()) if not return ""
+//Create hashmap that takes in chars and has indegree value
+//Create hashmap that takes in chars and has list of elements it points to 
+//Perform Khan's algorithm
+
+
 #include <string>
 #include <vector>
 #include <unordered_map>

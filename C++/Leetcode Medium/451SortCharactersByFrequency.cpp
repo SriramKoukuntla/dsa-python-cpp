6cdcde5ba6b88f01618695ec70b1eccfb29806a5
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2026-03-15 18:48:32 -0400

    451SortCharactersByFrequencyCpp

--- C++/Leetcode Medium/451SortCharactersByFrequency.cpp
+++ C++/Leetcode Medium/451SortCharactersByFrequency.cpp
@@ -1,6 +1,28 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        //Count frequency of each character
+        unordered_map<char, int> freq;
+        for (char c : s) ++freq[c];
+
+        //Sort
+        vector<vector<char>> sortedChars(s.size()+1); //index = frequency of characters
+        for (auto [letter, frequency] : freq) sortedChars[frequency].push_back(letter);
+
+        //Create res string
+        string res = "";
+        for (int i = sortedChars.size()-1; i >= 0; --i) for (char letter : sortedChars[i]) res += string(i, letter);
+        return res; 
+    }
+};
+//Count frequency of each character
+//Sort by ascending
+//Add strings to res based on their count
+
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <unordered_map>
 using namespace std;
 class Solution {
 public:

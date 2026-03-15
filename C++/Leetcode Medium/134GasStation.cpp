484214765677b8225d73bf5ed37b4386cdd04215
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2026-03-15 15:36:29 -0400

    134GasStationRedidCpp

--- C++/Leetcode Medium/134GasStation.cpp
+++ C++/Leetcode Medium/134GasStation.cpp
@@ -1,6 +1,72 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        //Create diff list
+        int sum = 0;
+        vector<int> diff;
+        for (int i = 0; i < gas.size(); ++i) {diff.push_back({gas[i]-cost[i]}); sum += gas[i]-cost[i];}
+        if (sum < 0) return -1;
+        
+
+        //Core algorithm
+        int res = -1;
+        int currGas = 0;
+        for (int i = 0; i < diff.size(); ++i) {
+            if (res == -1) res = i;
+            currGas += diff[i];
+            if (currGas < 0) {
+                currGas = 0;
+                res = -1;
+            }
+        }
+        return res; 
+    }
+};
+
+#include <vector>
+#include <list>
+using namespace std;
+
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        //Create diff list
+        int sum = 0;
+        list<tuple<int, int>> diff;
+        for (int i = 0; i < gas.size(); ++i) {diff.push_back({gas[i]-cost[i], i}); sum += gas[i]-cost[i];}
+        if (sum < 0) return -1;
+
+        //Combination filter
+        bool combined = true;
+        while (combined) {
+            combined = false;
+            for (auto it = diff.begin(); it != diff.end();) {
+                // for (auto [val, idx] : diff) cout << "(val: " << val << " idx: " << idx << "), ";
+                // cout << endl; 
+
+                //We only want to iterate from index [0, diff.size()-1]
+                auto next = it;
+                ++next;
+                if (next == diff.end()) break;
+
+                if (get<0>(*it) < 0 && get<0>(*next) > 0) it = next; //neg -> pos condition
+                else {
+                    combined = true;;
+                    get<0>(*it) += get<0>(*next);
+                    diff.erase(next);
+                }
+            }
+        }
+
+        //If size 1 case
+        if (diff.size() == 1) return (get<0>(diff.front()) < 0) ? -1 : get<1>(diff.front());
+        else return (get<0>(diff.front()) + get<0>(diff.back()) < 0) ? -1 : get<1>(diff.back());
+    }
+};
+
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
         vector<int> diff;
         int totalGas = 0;
         int totalCost = 0;

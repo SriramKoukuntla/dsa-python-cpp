953e7276026eec45cb01bd14ddf190a922e5ba8a
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2026-03-15 20:27:37 -0400

    78SubsetsRedidCpp

--- C++/Leetcode Medium/78Subsets.cpp
+++ C++/Leetcode Medium/78Subsets.cpp
@@ -1,3 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> res = {{}};
+        for (int i = 0; i < nums.size(); ++i) {
+            int n = res.size();
+            for (int j = 0; j < n; ++j) {
+                vector<int> temp = res[j];
+                temp.push_back(nums[i]);
+                res.push_back(temp);
+            }
+        }
+        return res; 
+    }
+};
+//Initialize res array as a singular empty {{}}
+//For each element in nums, grab each array in res that existed before the iteration started, and create a new temporary vector which is res[j] + nums[i] and put it onto res
+//return res;
+
+//{[]}
+//{[], [1]}
+//{[2], [1, 2]} -> {[], [1], [2], [1, 2]}
+//{[], [1], [2], [1, 2]} -> 
+//{ -> {[], [1], [2], [1, 2], [3], [1, 4], [2, 3], [1, 2, 3]} 
+
 #include <vector>
 using namespace std;
 class Solution {

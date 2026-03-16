b0098ba3ecd8ecf243729a9b8a409c50fc2647c8
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2026-03-15 23:28:01 -0400

    190ReverseBitsRedidCpp

--- C++/Leetcode Easy/190ReverseBits.cpp
+++ C++/Leetcode Easy/190ReverseBits.cpp
@@ -1,3 +1,16 @@
+class Solution {
+public:
+    int reverseBits(int n) {
+        int res = 0;
+        for (int i = 0; i < 32; ++i) {
+            res <<= 1; //shift bits to insert next bit
+            res += (n % 2);
+            n /= 2;
+        }
+        return res; 
+    }
+};
+
 using namespace std;
 
 class Solution {

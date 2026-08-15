14c210d86e42d997faf0a8b849b458ef386ce7ae
Sriram Koukuntla <Sriram.Koukuntla@gmail.com>
2026-08-15 18:01:06 -0500

    242ValidAnagramCpp

--- C++/Leetcode Easy/242ValidAnagram.cpp
+++ C++/Leetcode Easy/242ValidAnagram.cpp
@@ -3,6 +3,19 @@ using namespace std;
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        unordered_map<char, int> mapS;
+        unordered_map<char, int> mapT;
+        for (char c : s) mapS[c]++;
+        for (char c : t) mapT[c]++;
+        return mapS == mapT;
+    }
+};
+
+#include <unordered_map>
+using namespace std;
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
         unordered_map<char, int> map;
         for (char c : s) ++map[c];
         for (char c : t) --map[c];

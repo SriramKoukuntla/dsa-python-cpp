c8e5ecedaf63600a11ee649ebdff54764c29bd3f
Sriram Koukuntla <Sriram.Koukuntla@gmail.com>
2026-08-31 14:55:04 -0400

    225ImplementStackUsingQueuesCpp

--- C++/Leetcode Easy/225ImplementStackUsingQueues.cpp
+++ C++/Leetcode Easy/225ImplementStackUsingQueues.cpp
@@ -1,3 +1,198 @@
+#include <queue>
+using namespace std;
+class MyStack {
+public:
+    queue<int> q1;
+    queue<int> q2;
+    MyStack() {}
+    
+    void push(int x) {
+        if (q2.empty()) {
+            q2.push(x);
+            while (!q1.empty()) { 
+                q2.push(q1.front());
+                q1.pop();
+            }
+        }
+        else {
+            q1.push(x);
+            while (!q2.empty()) { 
+                q1.push(q2.front());
+                q2.pop();
+            }
+        }
+    }
+    
+    int pop() {
+        int temp = top();
+        if (!q2.empty()) q2.pop();
+        if (!q1.empty()) q1.pop();
+        return temp;
+    }
+    
+    int top() {
+        int temp = -1;
+        if (!q2.empty()) temp = q2.front();
+        if (!q1.empty()) temp = q1.front();
+        return temp;
+    }
+    
+    bool empty() {
+        return q1.empty() && q2.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
+ 
+#include <queue>
+using namespace std;
+class MyStack {
+public:
+    queue<int> q1; 
+    queue<int> q2;
+    MyStack() {}
+    
+    void push(int x) {
+        if (q2.empty()) {
+            q2.push(x);
+            while (!q1.empty()) {
+                q2.push(q1.front());
+                q1.pop();
+            }
+        }
+        else {
+            q1.push(x);
+            while (!q2.empty()) {
+                q1.push(q2.front());
+                q2.pop();
+            }
+        }
+    }
+    
+    int pop() {
+        int temp = top();
+        if (!q1.empty()) q1.pop();
+        if (!q2.empty()) q2.pop();
+        return temp;
+    }
+    
+    int top() {
+        if (!q1.empty()) return q1.front();
+        if (!q2.empty()) return q2.front();
+        return -1;
+    }
+    
+    bool empty() {
+        return q1.empty() && q2.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
+
+#include <queue>
+using namespace std;
+class MyStack {
+public:
+    queue<int> q1; 
+    queue<int> q2;
+    MyStack() {}
+    
+    void push(int x) {
+        if (!q2.empty()) swap(q1, q2);
+
+        q2.push(x);
+        while (!q1.empty()) {
+            q2.push(q1.front());
+            q1.pop();
+        }
+    }
+    
+    int pop() {
+        int temp = top();
+        if (!q1.empty()) q1.pop();
+        if (!q2.empty()) q2.pop();
+        return temp;
+    }
+    
+    int top() {
+        if (!q1.empty()) return q1.front();
+        if (!q2.empty()) return q2.front();
+        return -1;
+    }
+    
+    bool empty() {
+        return q1.empty() && q2.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
+
+#include <queue>
+using namespace std;
+class MyStack {
+public:
+    queue<int> q1;
+    queue<int> q2;
+    MyStack() {}
+    
+    void push(int x) {
+        if (!q1.empty()) swap(q1, q2);
+
+        q1.push(x);
+        while(!q2.empty()) {
+            q1.push(q2.front());
+            q2.pop();
+        }
+    }
+    
+    int pop() {
+        int temp = top();
+        if (!q1.empty()) q1.pop();
+        if (!q2.empty()) q2.pop();
+        return temp;
+    }
+    
+    int top() {
+        if (!q1.empty()) return q1.front();   
+        if (!q2.empty()) return q2.front();     
+        return -1;  
+    }
+    
+    bool empty() {
+        return q1.empty() && q2.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
+
 #include <list>
 using namespace std;
 class MyStack {

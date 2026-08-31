bfded134d2f58e69e313d8ad49652299a46ea125
Sriram Koukuntla <Sriram.Koukuntla@gmail.com>
2026-08-31 17:58:29 -0400

    35SearchInsertPositionCpp

--- C++/Leetcode Easy/35SearchInsertPosition.cpp
+++ C++/Leetcode Easy/35SearchInsertPosition.cpp
@@ -1,3 +1,20 @@
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int l = 0; 
+        int r = nums.size()-1;
+        while (l <= r) {
+            int m = l + (r-l)/2;
+            if (nums[m] == target) return m;
+            else if (nums[m] > target) r = m-1;
+            else if (nums[m] < target) l = m+1;
+        }
+        return l;
+    }
+};
+
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {

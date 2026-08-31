2e801a888cf57c3e4274c8b2273cb1fd5d12cf1e
Sriram Koukuntla <Sriram.Koukuntla@gmail.com>
2026-08-31 17:40:34 -0400

    704BinarySearchCpp

--- C++/Leetcode Easy/704BinarySearch.cpp
+++ C++/Leetcode Easy/704BinarySearch.cpp
@@ -1,3 +1,20 @@
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l = 0;
+        int r = nums.size()-1;
+        while (l <= r) {
+            int m = l + (r-l)/2;
+            if (nums[m] == target) return m;
+            if (nums[m] > target) r = m-1;
+            else l = m+1;
+        }
+        return -1;
+    }
+};
+
 #include <vector> 
 include namespace std;
 class Solution {

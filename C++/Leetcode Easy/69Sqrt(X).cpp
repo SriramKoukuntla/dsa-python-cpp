fb759475943c8d87ad8e6e46f896e75981447524
Sriram Koukuntla <Sriram.Koukuntla@gmail.com>
2026-08-31 18:44:08 -0400

    69Sqrt(X)Cpp

--- C++/Leetcode Easy/69Sqrt(X).cpp
+++ C++/Leetcode Easy/69Sqrt(X).cpp
@@ -1,3 +1,48 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int l = 0; 
+        int r = x;
+        int res = 0;
+        while (l <= r) {
+            int m = l + (r-l)/2;
+            unsigned long long sq = (unsigned long long)m * m;
+            if (sq == x) return m;
+            else if (sq < x) {
+                res = m;
+                l = m+1;
+            }
+            else if (sq > x) {
+                r = m-1;
+            }
+        }
+        return res;
+    }
+};
+
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x == 1) return 1;
+        int l = 1; 
+        int r = x/2;
+        int res = 0;
+        while (l <= r) {
+            int m = l + (r-l)/2;
+            unsigned long long sq = (unsigned long long)m * m;
+            if (sq == x) return m;
+            else if (sq < x) {
+                res = m;
+                l = m+1;
+            }
+            else if (sq > x) {
+                r = m-1;
+            }
+        }
+        return res;
+    }
+};
+
 #include <algorithm>
 using namespace std;
 class Solution {

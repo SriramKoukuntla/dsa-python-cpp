533f72f7af14a3327e6e3c39e9bf9f7735015dd0
Sriram Koukuntla <Sriram.Koukuntla@gmail.com>
2026-08-31 12:36:43 -0400

    1768MergeStringAlternativelyCpp

--- C++/Leetcode Easy/1768MergeStringAlternatively.cpp
+++ C++/Leetcode Easy/1768MergeStringAlternatively.cpp
@@ -3,6 +3,23 @@ using namespace std;
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
+        string combined;
+        for (int i = 0; i < min(word1.size(), word2.size()); ++i) {
+            combined += word1[i];
+            combined += word2[i];
+        }
+        if (word1.size() > word2.size()) combined.insert(combined.end(), word1.begin() + word2.size(), word1.end());
+        if (word1.size() < word2.size()) combined.insert(combined.end(), word2.begin() + word1.size(), word2.end());
+
+        return combined;
+    }
+};
+
+#include <string>
+using namespace std;
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
         string res = "";
         for (int i = 0; i < min(word1.size(), word2.size()); ++i) {
             res += word1[i];

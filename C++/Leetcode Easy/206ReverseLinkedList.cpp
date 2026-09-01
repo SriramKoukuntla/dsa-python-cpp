5479e5ce8c43cb53895ec2a52935abe2dd376e88
Sriram Koukuntla <Sriram.Koukuntla@gmail.com>
2026-09-01 12:56:28 -0400

    206ReverseLinkedListCpp

--- C++/Leetcode Easy/206ReverseLinkedList.cpp
+++ C++/Leetcode Easy/206ReverseLinkedList.cpp
@@ -19,6 +19,30 @@ public:
         ListNode* next = curr->next;
         curr->next = prev;
         return helper(next, curr);
+    }
+};
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        return helper(head, nullptr);
+    }
+
+    ListNode* helper(ListNode* curr, ListNode* prev) {
+        if (curr == nullptr) return prev;
+        ListNode* next = curr->next;
+        curr->next = prev;
+        return helper(next, curr);
 
     }
 };

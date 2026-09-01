aa41692d5618ee4cb26a3256e9afac4213941e0b
Sriram Koukuntla <Sriram.Koukuntla@gmail.com>
2026-09-01 10:42:11 -0400

    219ContainsDuplicatesIICpp

--- C++/Leetcode Easy/219ContainsDuplicatesII.cpp
+++ C++/Leetcode Easy/219ContainsDuplicatesII.cpp
@@ -1,3 +1,20 @@
+#include <vector>
+#include <unordered_set>
+using namespace std;
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        int l = 0;
+        unordered_set<int> set;
+        for (int r = 0; r < nums.size(); ++r) {
+            if (set.find(nums[r]) != set.end()) return true;
+            set.insert(nums[r]);
+            if (r-l == k) set.erase(nums[l++]);
+        }
+        return false;
+    }
+};
+
 #include <unordered_set>
 class Solution {
 public:

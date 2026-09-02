4e752c00a51f0e8786674b8dcdf9a1640aca5ea5
Sriram Koukuntla <Sriram.Koukuntla@gmail.com>
2026-09-02 19:05:14 -0400

    145BinaryTreePostOrderTraversalCpp

--- C++/Leetcode Easy/145BinaryTreePostOrderTraversal.cpp
+++ C++/Leetcode Easy/145BinaryTreePostOrderTraversal.cpp
@@ -16,6 +16,42 @@ class Solution {
 public:
     vector<int> postorderTraversal(TreeNode* root) {
         vector<int> result;
+        if (!root) return result;
+        stack<TreeNode*> st;
+        st.push(root);
+        while (!st.empty()) {
+            TreeNode* curr = st.top();
+            st.pop();
+            result.push_back(curr->val);
+            if (curr->left) st.push(curr->left);
+            if (curr->right) st.push(curr->right);
+
+        }
+
+        reverse(result.begin(), result.end());
+        return result;
+
+    }
+};
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+#include <vector>
+#include <stack>
+using namespace std;
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> result;
         stack<TreeNode*> sk;
         stack<bool> visit;
         sk.push(root);

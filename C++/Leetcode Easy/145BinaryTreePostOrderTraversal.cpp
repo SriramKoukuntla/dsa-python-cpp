76d69c0c21c80673158e9f581dfb256fb455483e
Sriram Koukuntla <Sriram.Koukuntla@gmail.com>
2026-09-02 18:01:50 -0400

    145BinaryTreePostOrderTraversalCpp

---
+++ C++/Leetcode Easy/145BinaryTreePostOrderTraversal.cpp
@@ -0,0 +1,67 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+#include <vector>
+#include <stack>
+using namespace std;
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> res;
+        stack<TreeNode*> sk;
+        stack<bool> visited;
+        sk.push(root);
+        visited.push(false);
+        while (!sk.empty()) {
+            TreeNode* curr = sk.top();
+            bool visit = visited.top();
+            sk.pop();
+            visited.pop();
+            if (curr == nullptr) continue;
+            if (visit) res.push_back(curr->val);
+            else {
+                sk.push(curr);
+                visited.push(true);
+                sk.push(curr->right);
+                visited.push(false);
+                sk.push(curr->left);
+                visited.push(false);
+            }
+        }
+        return res;
+    }
+};
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        if (!root) return {};
+        vector<int> res;
+        vector<int> left = postorderTraversal(root->left);
+        res.insert(res.end(), left.begin(), left.end());
+        vector<int> right = postorderTraversal(root->right);
+        res.insert(res.end(), right.begin(), right.end());
+        res.push_back(root->val);
+        return res;
+    }
+};

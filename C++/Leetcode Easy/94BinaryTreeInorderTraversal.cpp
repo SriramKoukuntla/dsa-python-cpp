ee76f1db2eb5a4eabd88a88cdbcfb70e45c8933d
Sriram Koukuntla <Sriram.Koukuntla@gmail.com>
2026-09-02 18:32:59 -0400

    94BinaryTreeInorderTraversalCpp

--- C++/Leetcode Easy/94BinaryTreeInorderTraversal.cpp
+++ C++/Leetcode Easy/94BinaryTreeInorderTraversal.cpp
@@ -15,6 +15,39 @@ using namespace std;
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> result;
+        stack<TreeNode*> sk;
+        TreeNode* curr = root;
+        while (curr || !sk.empty()) {
+            while (curr) {
+                sk.push(curr);
+                curr = curr->left;
+            }
+            curr = sk.top();
+            sk.pop();
+            result.push_back(curr->val);
+            curr = curr->right;
+        }
+        return result;
+    }
+};
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+#include <vector>
+#include <stack>
+using namespace std;
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
         vector<int> res;
         stack<TreeNode*> st;
         TreeNode* curr = root;

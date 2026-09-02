6c7d74a1c8fa9adfce99faf29b5057eec82eeb11
Sriram Koukuntla <Sriram.Koukuntla@gmail.com>
2026-09-02 14:27:20 -0400

    94BinaryTreeInorderTraversalCpp

--- C++/Leetcode Easy/94BinaryTreeInorderTraversal.cpp
+++ C++/Leetcode Easy/94BinaryTreeInorderTraversal.cpp
@@ -9,6 +9,76 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+#include <vector>
+#include <stack>
+using namespace std;
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> res;
+        stack<TreeNode*> st;
+        TreeNode* curr = root;
+        while (!st.empty() || curr) {
+            while (curr) {
+                st.push(curr);
+                curr = curr->left;
+            }
+            curr = st.top();
+            st.pop();
+            res.push_back(curr->val);
+            curr = curr->right;
+        }
+        return res;
+    }
+};
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+#include <vector>
+#include <stack>
+using namespace std;
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> res;
+        stack<TreeNode*> st;
+        TreeNode* curr = root;
+        while (!st.empty() || curr) {
+            //go to the left most
+            while (curr) {
+                st.push(curr);
+                curr = curr->left;
+            }
+            //proccess current node
+            curr = st.top();
+            st.pop();
+            res.push_back(curr->val);
+            //add right node to stack
+            curr = curr->right;
+        }
+        return res;
+    }
+};
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
  #include <vector>
  using namespace std;
 class Solution {

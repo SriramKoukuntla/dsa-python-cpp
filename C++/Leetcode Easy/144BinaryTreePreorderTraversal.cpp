26bbb9decafc1b85bdd50c11ad08c8329e99ce87
Sriram Koukuntla <Sriram.Koukuntla@gmail.com>
2026-09-02 14:41:39 -0400

    144BinaryTreePreorderTraversalCpp

--- C++/Leetcode Easy/144BinaryTreePreorderTraversal.cpp
+++ C++/Leetcode Easy/144BinaryTreePreorderTraversal.cpp
@@ -10,6 +10,37 @@
  * };
  */
 #include <vector>
+#include <stack>
+using namespace std;
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> res;
+        stack<TreeNode*> st;
+        st.push(root);
+        while (!st.empty()) {
+            TreeNode* curr = st.top();
+            st.pop();
+            if (!curr) continue;
+            res.push_back(curr->val);
+            st.push(curr->right);
+            st.push(curr->left);
+        }
+        return res;
+    }
+};
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+#include <vector>
 using namespace std;
 class Solution {
 public:

acf560828cb72d819d03361c30c35ef12e01ebeb
Sriram Koukuntla <Sriram.Koukuntla@gmail.com>
2026-09-05 13:21:39 -0400

    39CombinationSumCpp

--- C++/Leetcode Medium/39CombinationSum.cpp
+++ C++/Leetcode Medium/39CombinationSum.cpp
@@ -1,3 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> subSets;
+        vector<int> temp;
+        helper(subSets, temp, 0, candidates, target, 0);
+        return subSets;
+    }
+    void helper(vector<vector<int>>& subSets, vector<int>& temp, int sum, vector<int>& candidates, int target, int index) {
+        if (sum > target) return;
+        if (index == candidates.size()) return;
+        if (sum == target) {
+            subSets.push_back(temp);
+            return;
+        }
+
+        //don't choose branch
+        helper(subSets, temp, sum, candidates, target, index+1);
+
+        //choose branch
+        temp.push_back(candidates[index]);
+        sum += candidates[index];
+        helper(subSets, temp, sum, candidates, target, index);
+        temp.pop_back();
+        sum -= candidates[index];
+    }
+};
+
 #include <vector>
 using namespace std;
 class Solution {

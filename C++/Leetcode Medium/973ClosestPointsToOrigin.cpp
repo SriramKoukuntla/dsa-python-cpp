29e1d26412bc0f4217058607884e1f7cc5612604
Sriram Koukuntla <Sriram.Koukuntla@gmail.com>
2026-09-08 19:01:29 -0400

    973ClosestPointsToOriginCpp

--- C++/Leetcode Medium/973ClosestPointsToOrigin.cpp
+++ C++/Leetcode Medium/973ClosestPointsToOrigin.cpp
@@ -1,3 +1,28 @@
+#include <vector>
+#include <queue>
+using namespace std;
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        priority_queue<vector<double>> pq;
+        for (vector<int> point : points) {
+            double distance = sqrt(point[0] * point[0] + point[1] * point[1]); 
+            vector<double> temp = {distance, (double)point[0], (double)point[1]};
+            pq.push(temp);
+            if (pq.size() > k) pq.pop();
+        }
+        
+        vector<vector<int>> res;
+        while (!pq.empty()) {
+            vector<double> curr = pq.top();
+            res.push_back({(int)curr[1], (int)curr[2]});
+            pq.pop();
+        }
+
+        return res;
+    }
+};
+
 #include <queue>
 
 class Solution {

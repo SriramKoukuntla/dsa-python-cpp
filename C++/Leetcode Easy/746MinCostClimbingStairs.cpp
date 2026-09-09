98cb8639cf855c2203e25d666614e36fac62c761
Sriram Koukuntla <Sriram.Koukuntla@gmail.com>
2026-09-09 00:11:15 -0400

    746MinCostClimbingStairsCpp

--- C++/Leetcode Easy/746MinCostClimbingStairs.cpp
+++ C++/Leetcode Easy/746MinCostClimbingStairs.cpp
@@ -4,6 +4,21 @@ using namespace std;
 class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
+        vector<int> tab(cost.size(), 0);
+        tab[tab.size()-1] = cost[cost.size()-1];
+        tab[tab.size()-2] = cost[cost.size()-2];
+
+        for (int i = tab.size()-3; i >= 0; --i) tab[i] = cost[i] + min(tab[i+1], tab[i+2]);
+        return min(tab[0], tab[1]);
+    }
+};
+
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
         for (int i = 2; i < cost.size(); ++i){
             cost[i] = min(cost[i-2], cost[i-1]) + cost[i];
         }

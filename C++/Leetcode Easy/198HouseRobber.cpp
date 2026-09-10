9adac270f2e06fb5c8c81982edfa5225bc31ad84
Sriram Koukuntla <Sriram.Koukuntla@gmail.com>
2026-09-09 20:42:02 -0400

    198HouseRobberCpp

--- C++/Leetcode Easy/198HouseRobber.cpp
+++ C++/Leetcode Easy/198HouseRobber.cpp
@@ -2,6 +2,26 @@
 using namespace std;
 class Solution {
 public:
+    vector<int> mem;
+    int rob(vector<int>& nums) {
+        mem = vector<int>(nums.size(), -1);
+        return helper(nums, nums.size()-1);
+    }
+
+    int helper(vector<int>& nums, int i) {
+        if (i == 0) return nums[0];
+        if (i == 1) return max(nums[0], nums[1]);
+        if (mem[i] != -1) return mem[i];
+        int res = max(helper(nums, i-2) + nums[i], helper(nums, i-1));
+        mem[i] = res;
+        return res;
+    }
+};
+
+#include <vector>
+using namespace std;
+class Solution {
+public:
     int rob(vector<int>& nums) {
         if (nums.size() == 1) return nums[0];
         vector<int> tab(nums.size(), 0);

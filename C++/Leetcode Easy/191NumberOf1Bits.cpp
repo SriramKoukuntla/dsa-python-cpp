16bf2f5fa988932c9ed2e4fed2737ac281786d25
Sriram Koukuntla <Sriram.Koukuntla@gmail.com>
2026-09-17 21:54:04 -0400

    191NumberOf1Bits

--- C++/Leetcode Easy/191NumberOf1Bits.cpp
+++ C++/Leetcode Easy/191NumberOf1Bits.cpp
@@ -3,6 +3,30 @@ public:
     int hammingWeight(int n) {
         int res = 0;
         while (n != 0) {
+            n &= n-1;
+            ++res;
+        }
+        return res;
+    }
+};
+
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int res = 0;
+        while (n != 0) {
+            if (n & 1 == 1) ++res;
+            n = n >> 1;
+        }
+        return res; 
+    }
+};
+
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int res = 0;
+        while (n != 0) {
             if (n & 1 == 1) ++res;
             n = n >> 1;
         }

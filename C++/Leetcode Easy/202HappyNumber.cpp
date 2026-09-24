94d008cf2f11a0e1508a65fe208625096f36d43a
Sriram Koukuntla <Sriram.Koukuntla@gmail.com>
2026-09-24 19:28:37 -0400

    202HappyNumberCpp

--- C++/Leetcode Easy/202HappyNumber.cpp
+++ C++/Leetcode Easy/202HappyNumber.cpp
@@ -1,3 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool isHappy(int n) {
+        unordered_set<int> seen;
+        while (n != 1) {
+            if (seen.find(n) != seen.end()) return false;
+            seen.insert(n);
+            int temp = 0;
+            while (n != 0) {
+                temp += (n % 10) * (n % 10);
+                n /= 10;
+            }
+            n = temp;
+        }
+        return true;
+    }
+};
+
 class Solution {
 public:
     bool isHappy(int n) {

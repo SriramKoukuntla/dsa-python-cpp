e48a61fbb5316c3117899b78001230b12a28ef89
Sriram Koukuntla <Sriram.Koukuntla@gmail.com>
2026-09-24 14:56:09 -0400

    867TransposeMatrixCpp

---
+++ C++/Leetcode Easy/867TransposeMatrix.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        vector<vector<int>> res(matrix[0].size(), vector<int>(matrix.size(), -1));
+        for (int i = 0; i < matrix.size(); ++i) {
+            for (int j = 0; j < matrix[0].size(); ++j) res[j][i] = matrix[i][j];
+        }
+        return res;
+    }
+};
